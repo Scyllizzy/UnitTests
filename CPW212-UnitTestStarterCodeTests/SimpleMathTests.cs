771646d14844ea77b29aaa737c609d45269d8d15
Scyllizzy <babytink2002@hotmail.com>
2020-02-26 13:07:12 -0800

    Fixed Multiply_TwoNumbers_ReturnsProduct test method to be more readable

--- CPW212-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW212-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -29,40 +29,21 @@ namespace CPW212_UnitTestStarterCode.Tests
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
         {
             // Arrange
-            double pair1Num1 = 2;
-            double pair1Num2 = 5;
-
-            double pair2Num1 = -2;
-            double pair2Num2 = 5;
-
-            double pair3Num1 = 4.5;
-            double pair3Num2 = 2;
-
-            double pair4Num1 = -0;
-            double pair4Num2 = 0;
-
-
-            // Expected answers
-            double pair1ExpectedProduct = pair1Num1 * pair1Num2;
-            double pair2ExpectedProduct = pair2Num1 * pair2Num2;
-            double pair3ExpectedProduct = pair3Num1 * pair3Num2;
-            double pair4ExpectedProduct = pair4Num1 * pair4Num2;
+            // Expected answer
+            double expectedProduct = num1 * num2;
 
             // Act
-            // Method answers
-            double pair1MethodProduct = SimpleMath.Multiply(pair1Num1, pair1Num2);
-            double pair2MethodProduct = SimpleMath.Multiply(pair2Num1, pair2Num2);
-            double pair3MethodProduct = SimpleMath.Multiply(pair3Num1, pair3Num2);
-            double pair4MethodProduct = SimpleMath.Multiply(pair4Num1, pair4Num2);
+            double methodProduct = SimpleMath.Multiply(num1, num2);
 
             // Assert
-            Assert.AreEqual(pair1ExpectedProduct, pair1MethodProduct);
-            Assert.AreEqual(pair2ExpectedProduct, pair2MethodProduct);
-            Assert.AreEqual(pair3ExpectedProduct, pair3MethodProduct);
-            Assert.AreEqual(pair4ExpectedProduct, pair4MethodProduct);
+            Assert.AreEqual(expectedProduct, methodProduct);
         }
 
         [TestMethod]
